2a083f65595fb166ca26411c3a3455bcfd5370ea
Mikael <174565360+Mikael92002@users.noreply.github.com>
2025-02-23 22:03:39 -0600

    Implemented drawing functionality. In process of implementing functionality to change color

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const body = document.querySelector("body");
-body.style.display = "flex";
-body.style.flexDirection = "column";
-body.style.alignItems = "center";
-body.style.justifyContent = "center"
-body.style.flexShrink = "1";
+const masterDiv = document.querySelector(".master-div");
+masterDiv.style.display = "flex";
+
+// masterDiv.style.flexShrink = "1";
+
 
 const button = document.querySelector(".button-prompt");
 
@@ -22,7 +21,7 @@ let parentDivArray = [];
 function makeGrid(numOfSquares){
     if(parentDivArray.length>0){
         for(let k = 0;k<parentDivArray.length;k++){
-            body.removeChild(parentDivArray[k]);
+            masterDiv.removeChild(parentDivArray[k]);
         }
         parentDivArray.splice(0,parentDivArray.length-1);
     }
@@ -32,12 +31,24 @@ function makeGrid(numOfSquares){
     let parentDivArrayCounter = 0;
 
 
+
 for(let i = 0;i<=gridSize;i++){
     const div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
-    div.style.border = "1px solid red";
-    div.style.margin = "2px";
+    div.style.width = "6.4px";
+    div.style.height = "6.4px";
+    // div.style.border = "1px solid red";
+
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "blue";
+    })
+    div.addEventListener("mouseout", () =>{
+        if(div.style.color == "white"){
+        div.style.backgroundColor = "white";}
+    })
+    div.addEventListener("mousedown", () =>{
+        div.style.backgroundColor = "white";
+    })
+    
     divArray[i] = div;
 
     if(i % numOfSquares == 0 && i>0){
@@ -45,14 +56,18 @@ for(let i = 0;i<=gridSize;i++){
         parentDiv.style.display = "flex";
         parentDiv.style.alignItems = "center";
         parentDiv.style.justifyContent = "center";
-        const body = document.querySelector("body");
+        const masterDiv = document.querySelector(".master-div");
         for(let j = i-numOfSquares;j<i;j++){
             parentDiv.appendChild(divArray[j]);
         }
-        body.appendChild(parentDiv);
+        masterDiv.appendChild(parentDiv);
         parentDivArray[parentDivArrayCounter] = parentDiv;
         parentDivArrayCounter++;
     }
+}
+
+function divHover(){
+
 }
 
 gridMade = true;
